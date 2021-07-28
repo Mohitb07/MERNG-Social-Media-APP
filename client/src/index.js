d398d2f2bf482e5e9c59b2927b7aa897bab4e6a9
mohitb07 <bmohit980@gmail.com>
2021-07-28 14:31:51 +0530

    react(client) side added

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import ApolloProvider from './ApolloProvider'
+
+ReactDOM.render(
+    ApolloProvider,
+  document.getElementById('root')
+);
