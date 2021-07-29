cfcfd57e845677b7a49a56397786e4dcdb48d8c5
mohitb07 <bmohit980@gmail.com>
2021-07-29 21:48:28 +0530

    react-router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+      <Container>
+        <Navbar/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
