a47c2108a8556d12463f74ac75e73d711de2a824
mohitb07 <bmohit980@gmail.com>
2021-07-30 21:27:58 +0530

    initial render error fixed by providing inital empty object and grid value value changed to 1

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,9 +5,10 @@ import { Grid } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
 
 function Home() {
-    const {loading, data: {getPosts: posts}} = useQuery(FETCH_POSTS_QUERY)
+    const {loading, data: {getPosts: posts} = {}} = useQuery(FETCH_POSTS_QUERY)
+    console.log()
     return (
-        <Grid columns={3}>
+        <Grid columns={1}>
             <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
