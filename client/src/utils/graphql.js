8c7c8a3eca43c5b95842c90911afc0d8e06fa34d
mohitb07 <bmohit980@gmail.com>
2021-08-02 21:43:55 +0530

    POST FETCHING logic

---
+++ client/src/utils/graphql.js
@@ -0,0 +1,16 @@
+import gql from 'graphql-tag'
+
+export const FETCH_POSTS_QUERY = gql`
+    { 
+        getPosts {
+            id body createdAt username likeCount
+            commentCount
+            likes {
+                username
+            }
+            comments {
+                id username createdAt body
+            }
+        }
+    }
+`
