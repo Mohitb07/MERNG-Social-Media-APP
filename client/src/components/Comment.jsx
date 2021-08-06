42afe868bdfeb6b86ff81b4643b6c6ba21b7cfc6
mohitb07 <bmohit980@gmail.com>
2021-08-06 21:18:28 +0530

    posting comment feature now working

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,43 +1,80 @@
-import React, {useContext} from 'react'
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import React, {useContext, useRef, useState} from 'react'
+import { Button, Comment, Form, Header, Transition } from 'semantic-ui-react'
 import moment from 'moment'
 import DeleteButton from './DeleteButton'
+import { gql, useMutation } from '@apollo/client'
 
 import { AuthContext } from '../context/auth'
 
 const CommentExampleComment = ({comments, username, postId}) => {
   const {user} = useContext(AuthContext)
-  console.log('user', user.username)
-  console.log('username', username)
+  const commentInputRef = useRef(null)
+  const [commentContent, setCommentContent] = useState('')
+  const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+    update(){
+      setCommentContent('')
+      commentInputRef.current.blur()
+    },
+    variables:{
+      postId,
+      body:commentContent
+    }
+  })
+
+  
   return (<Comment.Group>
     <Header as='h3' dividing>
       Comments
     </Header>
     {comments.map(comment => (
-        <Comment key={comment.id}>
-          <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
-          <Comment.Content>
-            <Comment.Author as='a'>{comment.username}</Comment.Author>
-            <Comment.Metadata>
-              <div>{moment(comment.createdAt).fromNow()}</div>
-            </Comment.Metadata>
-            <Comment.Text>{comment.body}</Comment.Text>
-              {user && user.username === username && (
-                <DeleteButton postId={postId} commentId={comment.id}/>   
-              )}
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
+        <Transition.Group>
+          <Comment key={comment.id}>
+            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+            <Comment.Content>
+              <Comment.Author as='a'>{comment.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{moment(comment.createdAt).fromNow()}</div>
+              </Comment.Metadata>
+              <Comment.Text>{comment.body}</Comment.Text>
+                {user && user.username === username && (
+                  <DeleteButton postId={postId} commentId={comment.id}/>   
+                )}
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        </Transition.Group>
     ))} 
-
-    <Form reply>
-      <Form.TextArea />
-      <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-    </Form>
+    {user && (
+       <Form reply>
+       <textarea ref={commentInputRef} style={{marginBottom:15, marginTop:20}} placeholder="Comment..." value={commentContent} onChange={event => setCommentContent(event.target.value) }/>
+       <Button 
+        type="submit"
+        disabled={commentContent.trim() === ''} 
+        content='Add Reply' 
+        labelPosition='left' 
+        icon='edit' 
+        primary 
+        onClick={submitComment}
+       />
+     </Form>
+    )}
+   
   </Comment.Group>
   )
 }
+
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation createComment($postId: ID!, $body: String!){
+    createComment(postId:$postId, body: $body){
+      id
+      comments{
+        id body createdAt username
+      }
+      commentCount
+    }
+  }
+`
 
 export default CommentExampleComment
