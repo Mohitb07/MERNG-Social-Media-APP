bccb072990922e020afaf4eafc24ab4087559281
mohitb07 <bmohit980@gmail.com>
2021-08-07 22:05:50 +0530

    user profile page added

---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, Grid, Image, Rail, Segment } from 'semantic-ui-react'
+
+function UserProfile() {
+    return (
+        <div style={{display:'flex', justifyContent:'center'}}>
+            <Container>
+            <Grid centered columns={3}>
+    <Grid.Column>
+      <Segment>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+
+        <Rail dividing position='left'>
+          <Segment textAlign="center"><Image src='https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg' size="medium" circular verticalAlign="middle"/>
+            <h3>Username</h3>
+          </Segment>
+        </Rail>
+
+        <Rail dividing position='right'>
+          <Segment>Right Rail Content</Segment>
+        </Rail>
+      </Segment>
+    </Grid.Column>
+  </Grid>
+            </Container>
+        </div>
+    )
+}
+
+export default UserProfile
