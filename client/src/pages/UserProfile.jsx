09c61a622cb653a43b3689a5e0d9f5530815725c
mohitb07 <bmohit980@gmail.com>
2021-08-13 21:43:31 +0530

    fetching user's associated posts

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag'
-import React from 'react'
-import { Container, Grid, Image, Loader, Rail, Segment } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Container, Dimmer, Grid, Image, Loader, Rail, Segment } from 'semantic-ui-react'
 import { useQuery } from '@apollo/client'
 
 import Feed from '../components/Feed'
@@ -9,29 +9,40 @@ import Feed from '../components/Feed'
 function UserProfile(props) {
     const userId = props.match.params.userId;
     console.log(userId)
-    const {data:{getUserPost:post} = {}, error} = useQuery(GET_USER_POSTS,{
+    const {error, loading, data:{getUserPost:post} = {}} = useQuery(GET_USER_POSTS,{
       variables:{
         userId
       }
     }) 
-    console.log('getUserpost', post)
     if(error){console.log('error', error.message)}
     let postMarkup;
-    if(!post){
+    if(loading ){
       postMarkup = <Loader active/>
     }else {
-      console.log('pst', post)
-
+      console.log('user profile', post)
+      
       postMarkup = (
         <div style={{display:'flex', justifyContent:'center'}}>
             <Container>
               <Grid centered columns={3}>
                 <Grid.Column>
                   <Segment>
-                    <Feed/>          
+                    {
+                      loading ? (
+                        <Dimmer inverted active>
+                          <Loader />
+                        </Dimmer>
+                      )
+                        : (
+                          post && post.map(post => (
+                            <Feed key={post.id} post={post}/>
+                          ))
+                        )
+                  }
+
                     <Rail dividing position='left'>
                       <Segment textAlign="center"><Image src='https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg' size="medium" circular verticalAlign="middle"/>
-                        {/* <h3>{username}</h3> */}
+                      {post && <h3>{post[0].username}</h3>}
                       </Segment>
                     </Rail>
 
@@ -49,11 +60,15 @@ function UserProfile(props) {
 }
 
 const GET_USER_POSTS = gql`
-  query($userId: ID!){
+  query getUserPost($userId: ID!){
     getUserPost(userId:$userId){
-      body
+        id
+        username
+        body
+        commentCount
+        createdAt
+    }
   }
-}
 `
 
 export default UserProfile
