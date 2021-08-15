d893b4c1822bcafd3c5a9745799f092f6619ff93
mohitb07 <bmohit980@gmail.com>
2021-08-15 21:06:14 +0530

    new responsive design for user profile page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -22,37 +22,30 @@ function UserProfile(props) {
       console.log('user profile', post)
       
       postMarkup = (
-        <div style={{display:'flex', justifyContent:'center'}}>
-            <Container>
-              <Grid centered columns={3}>
+        <div className="user-profile-container" style={{}}>
+          <Image className="image" src='https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg' size='medium' circular centered />
+          <div className="name">
+            {post && <h3>{post[0].username}</h3>}
+          </div>
+              <Grid columns={1}>
                 <Grid.Column>
                   <Segment>
-                    {
-                      loading ? (
-                        <Dimmer inverted active>
-                          <Loader />
-                        </Dimmer>
-                      )
-                        : (
-                          post && post.map(post => (
-                            <Feed key={post.id} post={post}/>
-                          ))
+                      {
+                        loading ? (
+                          <Dimmer inverted active>
+                            <Loader />
+                          </Dimmer>
                         )
-                  }
+                          : (
+                            post && post.map(post => (
+                              <Feed key={post.id} post={post}/>
+                            ))
+                          )
+                      }                   
 
-                    <Rail dividing position='left'>
-                      <Segment textAlign="center"><Image src='https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg' size="medium" circular verticalAlign="middle"/>
-                      {post && <h3>{post[0].username}</h3>}
-                      </Segment>
-                    </Rail>
-
-                    <Rail dividing position='right'>
-                      <Segment>Right Rail Content</Segment>
-                    </Rail>
                   </Segment>
                 </Grid.Column>
               </Grid>
-            </Container>
         </div>
       )
     }
