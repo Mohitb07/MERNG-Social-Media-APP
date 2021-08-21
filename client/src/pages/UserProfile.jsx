3085541b717841542b63cacc822c74f5fcf906ad
mohitb07 <bmohit980@gmail.com>
2021-08-21 22:00:31 +0530

    now shows total posts and follow unfollow button

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,12 +1,14 @@
 import gql from 'graphql-tag'
-import React from 'react'
-import { Container, Dimmer, Grid, Image, Loader, Segment } from 'semantic-ui-react'
+import React, { useContext } from 'react'
+import { Button, Container, Dimmer, Grid, Loader, Segment } from 'semantic-ui-react'
 import { useQuery } from '@apollo/client'
 
 import Feed from '../components/Feed'
-
+import { AuthContext } from '../context/auth';
+import Follow from '../components/Follow'
 
 function UserProfile(props) {
+    const {user}  = useContext(AuthContext)
     const userId = props.match.params.userId;
     console.log(userId)
     const {error, loading, data:{getUserPost:post} = {}} = useQuery(GET_USER_POSTS,{
@@ -22,22 +24,7 @@ function UserProfile(props) {
       console.log('user profile', post)
       
       postMarkup = (
-        <div className="user-profile-container" style={{}}>
-          {/* <Image className="image" src='https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg' size='medium' circular centered />
-          <div className="name">
-            {post && <h3>{post[0].username}</h3>}
-          </div> */}
-              {/* <div style={{display:"flex", justifyContent:'center', alignItems: 'center'}}>
-                            <div style={{width:'9rem'}}>
-                            <p>Followings</p>
-                            <h4>10</h4>
-                            </div>
-                            <div style={{width:'9rem'}}>
-                            <p>Followers</p>
-                            <h4>20</h4>
-                            </div>
-              </div> */}
-        
+        <div className="user-profile-container">        
         <div className="profileContainer">
            <section className="profile">
                 <header className="profileHeader">
@@ -47,13 +34,21 @@ function UserProfile(props) {
                         
                     
                      {post && <h3 className="heading">{post[0].username}</h3>}
+                     {
+                       post[0].username !== user.username ? <Follow id={userId}/> : <Button>Edit Profile &nbsp; <i class="sun outline icon"></i></Button>
+                     }
+                        
                     <div style={{display:"flex", justifyContent:'center', alignItems: 'center',marginTop:'2rem'}}>
                             <div style={{width:'9rem'}}>
-                            <p style={{fontSize:18}}>Followings</p>
+                            <p style={{fontSize:18}}>posts</p>
+                            <h2>{post.length}</h2>
+                            </div>
+                            <div style={{width:'9rem'}}>
+                            <p style={{fontSize:18}}>followers</p>
                             <h2>10</h2>
                             </div>
                             <div style={{width:'9rem'}}>
-                            <p style={{fontSize:18}}>Followers</p>
+                            <p style={{fontSize:18}}>following</p>
                             <h2>20</h2>
                             </div>
                     </div>
@@ -85,6 +80,7 @@ function UserProfile(props) {
                   </Segment>
                 </Grid.Column>
               </Grid>
+              <h1>User's Suggestion</h1>
               </Container>
               </div>
         </div>
