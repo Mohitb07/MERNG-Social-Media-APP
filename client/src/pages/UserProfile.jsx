19b0627ceafc10018823c3dfe0f7a17c440bf953
mohitb07 <bmohit980@gmail.com>
2021-08-24 21:57:25 +0530

    Fixed errors and code refactored

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,28 +1,37 @@
 import gql from 'graphql-tag'
 import React, { useContext } from 'react'
-import { Button, Container, Dimmer, Grid, Loader, Segment } from 'semantic-ui-react'
+import { Button, Container, Grid, Loader, Segment } from 'semantic-ui-react'
 import { useQuery } from '@apollo/client'
 
 import Feed from '../components/Feed'
 import { AuthContext } from '../context/auth';
 import Follow from '../components/Follow'
+import {GET_USER_QUERY} from '../utils/graphql'
 
 function UserProfile(props) {
     const {user}  = useContext(AuthContext)
     const userId = props.match.params.userId;
-    console.log(userId)
-    const {error, loading, data:{getUserPost:post} = {}} = useQuery(GET_USER_POSTS,{
+    
+    const {loading:userLoading, data: {getUser: userData} = {}} = useQuery(GET_USER_QUERY, {
+      variables: {
+        userId
+      }
+    })
+
+    const {loading:postLoading, data:{getUserPost:post} = {}} = useQuery(GET_USER_POSTS,{
       variables:{
         userId
       }
     }) 
-    if(error){console.log('error', error.message)}
+    
     let postMarkup;
-    if(loading ){
-      postMarkup = <Loader active/>
-    }else {
-      console.log('user profile', post)
-      
+
+    if (userLoading || postLoading) {
+      postMarkup = <Loader />
+    }else if (userData === null){
+      postMarkup = <h3>Not Found</h3>
+    }
+    else {
       postMarkup = (
         <div className="user-profile-container">        
         <div className="profileContainer">
@@ -33,9 +42,10 @@ function UserProfile(props) {
                      <img src="https://pbs.twimg.com/profile_images/1416573352358162446/vQPbSf9Z_400x400.jpg" alt="profile pic" className="profilePic" />                
                         
                     
-                     {post && <h3 className="heading">{post[0].username}</h3>}
+                     {/* {post && <h3 className="heading">{post[0].username}</h3>} */}
+                     <h3 className="heading">{userData.username}</h3>
                      {
-                       post[0].username !== user.username ? <Follow id={userId}/> : <Button>Edit Profile &nbsp; <i class="sun outline icon"></i></Button>
+                       post[0].username !== user.username ? <Follow userData={userData} id={userId}/> : <Button>Edit Profile &nbsp; <i class="sun outline icon"></i></Button>
                      }
                         
                     <div style={{display:"flex", justifyContent:'center', alignItems: 'center',marginTop:'2rem'}}>
@@ -45,11 +55,11 @@ function UserProfile(props) {
                             </div>
                             <div style={{width:'9rem'}}>
                             <p style={{fontSize:18}}>followers</p>
-                            <h2>10</h2>
+                            <h2>{userData.followers.length}</h2>
                             </div>
                             <div style={{width:'9rem'}}>
                             <p style={{fontSize:18}}>following</p>
-                            <h2>20</h2>
+                            <h2>{userData.followings.length}</h2>
                             </div>
                     </div>
                     </div>
@@ -64,18 +74,12 @@ function UserProfile(props) {
                   <Segment>
                     <h2 style={{textAlign: 'center'}}>Your Posts</h2>
                     <hr/>
-                      {
-                        loading ? (
-                          <Dimmer inverted active>
-                            <Loader />
-                          </Dimmer>
-                        )
-                          : (
-                            post && post.map(post => (
-                              <Feed key={post.id} post={post}/>
-                            ))
-                          )
-                      }                   
+                     {
+                       post && post.map(post => (
+                        <Feed key={post.id} post={post}/>
+                      ))
+                      }                       
+                        
 
                   </Segment>
                 </Grid.Column>
@@ -86,7 +90,11 @@ function UserProfile(props) {
         </div>
       )
     }
-    return postMarkup;
+    return (
+      <>      
+        {postMarkup}
+      </>
+    )
 }
 
 const GET_USER_POSTS = gql`
@@ -101,4 +109,4 @@ const GET_USER_POSTS = gql`
   }
 `
 
-export default UserProfile
+export default UserProfile;
