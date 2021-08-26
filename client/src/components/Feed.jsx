4d03b38cc020bf976e381f0e5fd8b9a8cdf8dcf4
mohitb07 <bmohit980@gmail.com>
2021-08-26 21:06:02 +0530

    code refactored

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -5,20 +5,20 @@ import {Link} from 'react-router-dom'
 
 const FeedExampleIconLabel = ({post:{body, createdAt, id}}) => (
   <Container>
-  <Feed>
-    <Feed.Event>
-      <Feed.Label>
-        <Icon name='pencil' />
-      </Feed.Label>
-      <Feed.Content>
-        <Feed.Date>{moment(createdAt).fromNow()}</Feed.Date>
-        <Feed.Summary>
-          <Link to={`/posts/${id}`}>{body}</Link>
-        </Feed.Summary>
-      </Feed.Content>
-    </Feed.Event>
-  </Feed>
-  </Container>
-)
+    <Feed>
+      <Feed.Event>
+        <Feed.Label>
+          <Icon name='pencil' />
+          </Feed.Label>
+          <Feed.Content>
+            <Feed.Date>{moment(createdAt).fromNow()}</Feed.Date>
+            <Feed.Summary>
+              <Link to={`/posts/${id}`}>{body}</Link>
+            </Feed.Summary>
+          </Feed.Content>
+        </Feed.Event>
+      </Feed>
+    </Container>
+  )
 
 export default FeedExampleIconLabel
